e30a71cb3864be633ecdcc6b88c976d654ba0583
tuanmjnh <minhtuan200990@gmail.com>
2024-11-06 22:19:32 +0700

    2024.11.06

--- tiktok_scrape.py
+++ tiktok_scrape.py
@@ -1,12 +1,38 @@
 import argparse
 import re
 import os
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 import requests
 from urllib.request import urlopen
+
+urlsFile = 'urls.txt'
+linksFolder = 'links'
+
+
+def pause(msg="Press the any key to continue..."): input(msg)
+
+
+def stop(msg="Stop Application."): sys.exit(msg)
+
+
+def ignore_exception(IgnoreException=Exception, DefaultVal=None):
+    """ Decorator for ignoring exception from a function
+    e.g.   @ignore_exception(DivideByZero)
+    e.g.2. ignore_exception(DivideByZero)(Divide)(2/0)
+    """
+    def dec(function):
+        def _dec(*args, **kwargs):
+            try:
+                return function(*args, **kwargs)
+            except IgnoreException:
+                return DefaultVal
+        return _dec
+    return dec
+
 
 def downloadVideo(link, id):
     print(f"Downloading video {id} from: {link}")
@@ -27,9 +53,9 @@ def downloadVideo(link, id):
     data = {
         'id': link,
         'locale': 'en',
-        'tt': '', # NOTE: This value gets changed, please use the value that you get when you copy the curl command from the network console
+        'tt': '',  # NOTE: This value gets changed, please use the value that you get when you copy the curl command from the network console
     }
-    
+
     print("STEP 4: Getting the download link")
     print("If this step fails, PLEASE read the steps above")
     response = requests.post('https://ssstik.io/abc', params=params, cookies=cookies, headers=headers, data=data)
@@ -49,6 +75,98 @@ def downloadVideo(link, id):
             else:
                 break
 
+
+def getLink(args):
+    # get channel key
+    regex = re.search(r'(?<=@)\w+', args.url)
+    channelName = regex.group(0)
+
+    # remove text links if exist
+    if os.path.exists(f"{channelName}.txt"):
+        os.remove(f"{channelName}.txt")
+    #   os.rmdir(f"{channelName}.txt")
+
+    # print("STEP 1: Open Chrome browser")
+    options = Options()
+    options.add_argument("start-maximized")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    driver = webdriver.Chrome(options=options)
+    # Change the tiktok link
+    # driver.get("https://www.tiktok.com/@vantoan___")
+    driver.get(args.url)
+
+    # IF YOU GET A TIKTOK CAPTCHA, CHANGE THE TIMEOUT HERE
+    # to 60 seconds, just enough time for you to complete the captcha yourself.
+    time.sleep(1)
+
+    # Count scroll
+    if args.count == None:
+        args.count = -1
+
+    # scroll_pause_time = 5
+    if (args.delay == None):
+        args.delay = 1
+    screen_height = driver.execute_script("return window.screen.height;")
+    i = 0
+
+    # print("STEP 2: Scrolling page")
+    while i > args.count:
+        driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
+        time.sleep(1)
+        scroll_height = driver.execute_script("return document.body.scrollHeight;")
+        time.sleep(args.delay)
+        # print(screen_height * i, ' - ', scroll_height)
+        i += 1
+        if (screen_height * i) > scroll_height:
+            break
+
+    # this class may change, so make sure to inspect the page and find the correct class
+    if (args.className == None):
+        args.className = "css-1uqux2o-DivItemContainerV2"
+
+    script = "let l = [];"
+    script += r"let regex = /(^|.*)https:\/\/www.tiktok.com\/@(.*)\/video\/(.*|$)/;"
+    script += "Array.from(document.getElementsByClassName(\""
+    script += args.className
+    # script += "\").forEach(item => { l.push(item.querySelector('a').href)});"
+    script += "\")).forEach(i=>{"
+    script += "var a = i.querySelector('a').href;"
+    script += "if(regex.test(a)) l.push(a);"
+    script += "});"
+    script += "return l;"
+
+    urlsToDownload = driver.execute_script(script)
+
+    # print(f"STEP 3: Time to download {len(urlsToDownload)} videos")
+    if (args.file > 0):
+        if args.count == -1:
+            for i, u in enumerate(urlsToDownload):
+                f = open(f"{linksFolder}/{channelName}.txt", "a")  # a append, w overwrite
+                if (i == len(urlsToDownload)-1):
+                    f.write(f"{u}")
+                else:
+                    f.write(f"{u}{args.split}")
+                f.close()
+        else:
+            for i, u in enumerate(urlsToDownload):
+                if i >= args.count:
+                    break
+                f = open(f"{linksFolder}/{channelName}.txt", "a")  # a append, w overwrite
+                if (i == len(urlsToDownload)-1):
+                    f.write(f"{u}")
+                else:
+                    f.write(f"{u}{args.split}")
+                f.close()
+    else:
+        print(u)
+        # print(f"Downloading video: {index}: {url}")
+        # downloadVideo(url, index)
+        # time.sleep(10)
+    # Close browser
+    driver.close()
+
+
 # Help --help
 # if(sys.argv[1]=="--help" or sys.argv[1]=="-h"):
     # print("Help Tools get all video from tiktok")
@@ -59,84 +177,60 @@ def downloadVideo(link, id):
     # print("--help or -h show help tool")
     # sys.exit()
 
+
 # parser input argument
 parser = argparse.ArgumentParser()
 parser.add_argument('--url', dest='url', type=str, help='url channel')
 parser.add_argument('--class', dest='className', type=str, help='class name content')
 parser.add_argument('--file', dest='file', type=int, help='write to file')
 parser.add_argument('--delay', dest='delay', type=int, help='delay scroll')
+parser.add_argument('--split', dest='split', type=str, help='split character')
 args = parser.parse_args()
 
 # Fix argument write file
-if(args.file==None): args.file=0
+if (args.file == None):
+    args.file = 0
+if (args.split == None):
+    args.split = '\n'
 # check input link channel
 # if(args.url==None):
 #     print("Please input link channel")
 #     sys.exit()
 
-# get channel key
-regex = re.search(r'(?<=@)\w+', args.url)
-channelName = regex.group(0)
+# Make folder links
+if os.path.exists(linksFolder) == False:
+    os.mkdir(linksFolder)
 
-# remove text links if exist
-if os.path.exists(f"{channelName}.txt"):
-  os.remove(f"{channelName}.txt")
-#   os.rmdir(f"{channelName}.txt")
 
-# print("STEP 1: Open Chrome browser")
-options = Options()
-options.add_argument("start-maximized")
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-driver = webdriver.Chrome(options=options)
-# Change the tiktok link
-# driver.get("https://www.tiktok.com/@vantoan___")
-driver.get(args.url)
+# If exist argument url
+if args.url != None:
+    getLink(args)
+    pause("Successful! Press the any key to close...")
+    stop()
+else:
+    # Check File urls
+    if os.path.exists(urlsFile) == False:
+        print("No exist file 'urls.txt'")
+        pause()
+        stop()
 
-# IF YOU GET A TIKTOK CAPTCHA, CHANGE THE TIMEOUT HERE
-# to 60 seconds, just enough time for you to complete the captcha yourself.
-time.sleep(1)
+    # Get File urls
+    txt = open(urlsFile, "r")
+    urls = txt.read().split('\n')
+    # urlsOpts = []
+    sint = ignore_exception(ValueError)(int)
+    for i, u in enumerate(urls):
+        # urlsOpts.append(url)
+        opts = u.split()  # re.split(r'\t+', u)
+        if (len(opts) > 1):
+            opts[1] = sint(opts[1])
+            # opts[1] = -1 if opts[1] == None else opts[1]
+            time.sleep(3)
+            args.url = opts[0]
+            args.count = opts[1]
+            getLink(args)
+        # urlsOpts.append(opts)
+    # print(urlsOpts)
 
-# scroll_pause_time = 5
-if(args.delay==None): args.delay=1
-screen_height = driver.execute_script("return window.screen.height;")
-i = 1
-
-# print("STEP 2: Scrolling page")
-while True:
-    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
-    i += 1
-    time.sleep(args.delay)
-    scroll_height = driver.execute_script("return document.body.scrollHeight;")  
-    if (screen_height) * i > scroll_height:
-        break 
-
-# this class may change, so make sure to inspect the page and find the correct class
-className = "tiktok-vi46v1-DivDesContainer eih2qak4"
-if(args.className!=None):
-    className = args.className
-
-script = "let l = [];"
-script += r"let regex = /(^|.*)https:\/\/www.tiktok.com\/@(.*)\/video\/(.*|$)/;"
-script += "document.getElementsByClassName(\""
-script += className
-# script += "\").forEach(item => { l.push(item.querySelector('a').href)});"
-script += "\").forEach(i=>{"
-script += "var a = i.querySelector('a').href;"
-script += "if(regex.test(a)) l.push(a);"
-script += "});"
-script += "return l;"
-
-urlsToDownload = driver.execute_script(script)
-
-# print(f"STEP 3: Time to download {len(urlsToDownload)} videos")
-for index, url in enumerate(urlsToDownload):
-    if(args.file>0):
-        f = open(f"{channelName}.txt", "a") # a append, w overwrite 
-        if(index==len(urlsToDownload)-1): f.write(f"{url}") 
-        else: f.write(f"{url}|")
-        f.close()
-    print(url)
-    # print(f"Downloading video: {index}: {url}")
-    # downloadVideo(url, index)
-    # time.sleep(10)
+    pause("Successful! Press the any key to close...")
+    stop()
